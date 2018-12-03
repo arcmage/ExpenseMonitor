570d2d435dda09ce11240d47f478fdda34f372c3
Mohammed Alquwaisem <arcmage666@live.com>
2018-12-03 16:12:52 +0300

    Initial Commit

---
+++ app/src/main/java/mq/expensemonitor/MainScreen.java
@@ -0,0 +1,13 @@
+package mq.expensemonitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
